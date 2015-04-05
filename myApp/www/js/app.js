92e97fe37a49252226624cc4e5d020fb49e4c010
sked1985 <mcgettiganbarry@yahoo.com>
2015-04-05 21:07:35 +0100

    Hide tabs
    Added functionality which hides the footer tabs on some pages because
    they are not required.

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -26,6 +26,21 @@ angular.module('starter', ['ionic',
   });
 })
 
+.directive('hideTabs', function($rootScope) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            scope.$watch(attributes.hideTabs, function(value){
+                $rootScope.hideTabs = value;
+            });
+
+            scope.$on('$destroy', function() {
+                $rootScope.hideTabs = false;
+            });
+        }
+    };
+})
+
 //This is the directive for the fake status bar on the app
 .directive('fakeStatusbar', function() {
   return {
